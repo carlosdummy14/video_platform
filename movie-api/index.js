bbb67638391f5e49642e437c476bba89da2a124d
carlos_linux <carlosdummy14@gmail.com>
2020-03-01 23:41:58 -0600

    Se agrego un middleware para manejar los errores a presentar cuando se esta en desarrollo y cuando se esta en produccion

--- movie-api/index.js
+++ movie-api/index.js
@@ -4,10 +4,18 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+// Middlewares de inicio
 // Body Parser Este middleware es para que las rutas sepan interpretar el formato json cuando se les envie
 app.use(express.json());
+// Errors
 
 moviesApi(app);
+
+// Middlewares finales
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
