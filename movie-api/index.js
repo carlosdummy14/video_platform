6adfaaa0e8309c1c8808bdaaf671fb9a80ef5c86
carlos_linux <carlosdummy14@gmail.com>
2020-03-02 00:14:01 -0600

    Se incluye libreria middleware @hapi/boom para el manejo de errores, se crea manejo de error 404

--- movie-api/index.js
+++ movie-api/index.js
@@ -4,17 +4,23 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // Middlewares de inicio
 // Body Parser Este middleware es para que las rutas sepan interpretar el formato json cuando se les envie
 app.use(express.json());
-// Errors
 
+// rutas
 moviesApi(app);
 
-// Middlewares finales
+//  Capturar error 404
+app.use(notFoundHandler);
+
+// Middlewares finales manejadores de errores
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
