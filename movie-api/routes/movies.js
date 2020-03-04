5391258aa9dd0ecbce8fc17c89ccbe792a4254a2
carlos_linux <carlosdummy14@gmail.com>
2020-03-03 20:55:23 -0600

    Se agrega libreria joi y se configura para validar datos el valiationHandler, se crea carpeta utils/schemas para meter las validaciones necesarias, se agregan los middlewares de validacion en las rutas

--- movie-api/routes/movies.js
+++ movie-api/routes/movies.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const MovieService = require('../services/movies');
 
+// Requerimos datos para usar validacion, los schemas y el manejador
+const { movieIdSchema, createMovieSchema, updateMovieSchema } = require('../utils/schemas/movies');
+const validationHandler = require('../utils/middleware/validationHandler');
+
 function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
@@ -24,7 +28,11 @@ function moviesApi(app) {
   });
 
   //  GET:movie una sola pelicula
-  router.get('/:movieId', async function(req, res, next) {
+  router.get('/:movieId', validationHandler({ movieId: movieIdSchema }, 'params'), async function(
+    req,
+    res,
+    next
+  ) {
     const { movieId } = req.params;
 
     try {
@@ -40,7 +48,7 @@ function moviesApi(app) {
   });
 
   //  POST creacion de pelicula
-  router.post('/', async function(req, res, next) {
+  router.post('/', validationHandler(createMovieSchema), async function(req, res, next) {
     const { body: movie } = req;
 
     try {
@@ -55,51 +63,65 @@ function moviesApi(app) {
   });
 
   //  PUT se actualiza una pelicula
-  router.put('/:movieId', async function(req, res, next) {
-    const { movieId } = req.params;
-    const { body: movie } = req;
+  router.put(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    validationHandler(updateMovieSchema),
+    async function(req, res, next) {
+      const { movieId } = req.params;
+      const { body: movie } = req;
 
-    try {
-      const updatedMovieId = await movieService.updateMovie({ movieId, movie });
-      res.status(200).json({
-        data: updatedMovieId,
-        message: 'movie updated'
-      });
-    } catch (err) {
-      next(err);
+      try {
+        const updatedMovieId = await movieService.updateMovie({ movieId, movie });
+        res.status(200).json({
+          data: updatedMovieId,
+          message: 'movie updated'
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
   //  PATCH se actualiza una parte de la pelicula
-  router.patch('/:movieId', async function(req, res, next) {
-    const { movieId } = req.params;
-    const { body: movie } = req;
+  router.patch(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    validationHandler(updateMovieSchema),
+    async function(req, res, next) {
+      const { movieId } = req.params;
+      const { body: movie } = req;
 
-    try {
-      const patchedMovieId = await movieService.patchMovie({ movieId, movie });
-      res.status(200).json({
-        data: patchedMovieId,
-        message: 'movie patched'
-      });
-    } catch (err) {
-      next(err);
+      try {
+        const patchedMovieId = await movieService.patchMovie({ movieId, movie });
+        res.status(200).json({
+          data: patchedMovieId,
+          message: 'movie patched'
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
   // DELETE elimina una pelicula
-  router.delete('/:movieId', async function(req, res, next) {
-    const { movieId } = req.params;
+  router.delete(
+    '/:movieId',
+    validationHandler({ movieId: movieIdSchema }, 'params'),
+    async function(req, res, next) {
+      const { movieId } = req.params;
 
-    try {
-      const deletedMovieId = await movieService.deleteMovie({ movieId });
-      res.status(200).json({
-        data: deletedMovieId,
-        message: 'movie deleted'
-      });
-    } catch (err) {
-      next(err);
+      try {
+        const deletedMovieId = await movieService.deleteMovie({ movieId });
+        res.status(200).json({
+          data: deletedMovieId,
+          message: 'movie deleted'
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 }
 
 module.exports = moviesApi;
---
+++ movie-api/utils/schemas/movies.js
@@ -0,0 +1,47 @@
+const joi = require('@hapi/joi');
+
+// Se crean los schemas para cada dato a validar
+const movieIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const movieTitleSchema = joi.string().max(80);
+const movieYearSchema = joi
+  .number()
+  .min(1888)
+  .max(2077);
+const movieCoverSchea = joi.string().uri();
+const movieDescriptionSchema = joi.string().max(300);
+const movieDurationSchema = joi
+  .string()
+  .min(1)
+  .max(300);
+const movieContentRatingSchema = joi.string().max(5);
+const movieSourceSchema = joi.string().uri();
+const movieTagsSchema = joi.array().items(joi.string().max(50));
+
+// Se crean los schemas globales para cada aplicacion, incluyendo los schemas individuales requeridos
+const createMovieSchema = {
+  title: movieTitleSchema.required(),
+  year: movieYearSchema.required(),
+  cover: movieCoverSchea.required(),
+  description: movieDescriptionSchema.required(),
+  duration: movieDurationSchema.required(),
+  contentRating: movieContentRatingSchema.required(),
+  movieSource: movieSourceSchema.required(),
+  tags: movieTagsSchema
+};
+
+const updateMovieSchema = {
+  title: movieTitleSchema,
+  year: movieYearSchema,
+  cover: movieCoverSchea,
+  description: movieDescriptionSchema,
+  duration: movieDurationSchema,
+  contentRating: movieContentRatingSchema,
+  movieSource: movieSourceSchema,
+  tags: movieTagsSchema
+};
+
+module.exports = {
+  movieIdSchema,
+  createMovieSchema,
+  updateMovieSchema
+};
