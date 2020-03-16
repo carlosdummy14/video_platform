aa458f36ad5e1d389f8310aa07093453505af5b8
carlos_linux <carlosdummy14@gmail.com>
2020-03-16 13:26:54 -0600

    Creamos componente pie de pagina con estilos.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
-import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import '../assets/styles/App.scss';
 
 const App = () => (
   <div className='App'>
@@ -19,6 +21,8 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Footer />
   </div>
 );
 
