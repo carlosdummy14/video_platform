801951c9fa5292d96a87a589a26a4ea005bddaab
carlos_linux <carlosdummy14@gmail.com>
2020-03-17 22:54:45 -0600

    Creamos carpeta y archivo de rutas para usar BrowserRouter para encapsular las rutas. Se utiliza Route para definir cada una de las rutas, renombrando App.jsx de containers para que se llame Home.jsx y no se confunda. Se actualiza el index.js para que tome la ruta en lugar del Home.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -9,9 +9,9 @@ import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3030/initialState';
+const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -49,4 +49,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
