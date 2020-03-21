a37659322b7bed21d755ae8e932323ca17181f2f
carlos_linux <carlosdummy14@gmail.com>
2020-03-20 22:46:05 -0600

    Implementamos el borrado de favoritos, en el reducer utilizamos una funcion filter sobre el array de myList para separar aquellos elementos que no sean iguales al id a eliminar, se agrega en el item el icono para borrar y se le da funcionalidad, se agrega en la destructuracion el id para poder enviarlo al reducer

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setFavorite } from '../actions';
+import { setFavorite, deleteFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = props => {
   const { id, cover, title, year, contentRating, duration } = props;
@@ -18,6 +19,10 @@ const CarouselItem = props => {
       duration,
     });
   };
+
+  const handleDeleteFavorite = itemId => {
+    props.deleteFavorite(itemId);
+  };
   return (
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
@@ -29,6 +34,12 @@ const CarouselItem = props => {
             src={plusIcon}
             alt='Plus Icon'
             onClick={handleSetFavorite}
+          />
+          <img
+            className='carousel-item__details--img'
+            src={removeIcon}
+            alt='Remove Icon'
+            onClick={() => handleDeleteFavorite(id)}
           />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
@@ -51,6 +62,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
   setFavorite,
+  deleteFavorite,
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
