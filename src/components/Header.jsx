46d48917988e95bb1a089c68c5b8eaa1f28f8841
carlos_linux <carlosdummy14@gmail.com>
2020-03-22 00:01:01 -0600

    Se realizan validaciones sobre el Header para saber si estamos logeados o no, se crea accion y reducer para manejar el logout y eliminarlo del estado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { logoutRequest } from '../actions';
 import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
@@ -10,6 +12,10 @@ const Header = props => {
   const { user } = props;
   //Con esto sabemos cuantos elementos tiene el objeto user
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = event => {
+    props.logoutRequest({});
+  };
   return (
     <header className='header'>
       <Link to='/'>
@@ -25,16 +31,30 @@ const Header = props => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href='/'>Cuenta</a>
-          </li>
-          <li>
-            <Link to='/login'>Iniciar Sesión</Link>
-          </li>
+          {hasUser ? (
+            <li>
+              <a href='/'>{user.name}</a>
+            </li>
+          ) : null}
+          {hasUser ? (
+            <li>
+              <Link to='#logout' onClick={handleLogout}>
+                Cerrar Sesión
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to='/login'>Iniciar Sesión</Link>
+            </li>
+          )}
         </ul>
       </div>
     </header>
   );
+};
+
+Header.prototype = {
+  user: PropTypes.object,
 };
 
 const mapStateToProps = state => {
@@ -42,4 +62,9 @@ const mapStateToProps = state => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps, null)(Header);
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
