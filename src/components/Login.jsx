4ce0a71bc2389cc2bc762b74cc9c4b874a7f63a1
carlos_linux <carlosdummy14@gmail.com>
2020-03-21 22:57:27 -0600

    Conectamos el formulario con redux, mandamos los datos del formulario al estado general y reenvio al login desde el handleSubmit, se corrigieron nombres del formulario y se agrego opcion de debug para ver el estado en el navegador

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
 
-const Login = () => {
+const Login = props => {
   const [form, setValues] = useState({
     email: '',
   });
@@ -18,7 +20,8 @@ const Login = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push('/');
   };
 
   return (
@@ -27,14 +30,14 @@ const Login = () => {
         <h2>Inicia sesión</h2>
         <form className='login__container--form' onSubmit={handleSubmit}>
           <input
-            name='name'
+            name='email'
             className='input'
             type='text'
             placeholder='Correo'
             onChange={handleInput}
           />
           <input
-            name='email'
+            name='password'
             className='input'
             type='password'
             placeholder='Contraseña'
@@ -69,4 +72,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
