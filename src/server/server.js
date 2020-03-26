7053e6c37730f57c57c3e1283bab726a8f5f3049
carlos_linux <carlosdummy14@gmail.com>
2020-03-25 22:57:43 -0600

    Instalamos nodemon para tener el refrezco automatico de la aplicacion en modo desarrollo, creamos archivo .env para tener las variables de entorno y utilizamos la libreria dotenv para su uso

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { EnvironmentPlugin } from 'webpack';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
 
+if (ENV === 'development') {
+  console.log('Development Config...');
+}
+
 app.get('*', (req, res) => {
+  console.log('Hola');
+
   res.send({
     hello: 'express',
   });
 });
 
-app.listen(3000, err => {
+app.listen(PORT, err => {
   if (err) console.log(err);
-  else console.log('Server running on port 3000');
+  else console.log(`Server running on port ${PORT}`);
 });
