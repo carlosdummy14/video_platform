3bbdf21ccfee68b21db10bc9683f63041db39ca2
carlos_linux <carlosdummy14@gmail.com>
2020-03-26 21:47:36 -0600

    Se instala webpack-dev-middleware y webpack-hot-middleware, configuramos los middleware en el server, modificamos los pluggins de los webpackconfig para agregar el hotmodulereplacement, se corrige ruta del index, se define el modo en el que corre la aplicacion, se agrega react-hot-loader, modificamos el entry point del proyecto, modificamos babelrc para agregar la seccion de pluggins y configurar la carga de react-hot-loader con babel

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,6 +10,22 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development Config...');
+  // Configuracion de webpack del proyecto
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  // Copiler para compilar la configuracion de webpack
+  const compiler = webpack(webpackConfig);
+  // Esta objeto se utiliza con el DevMiddleware
+  const serverConfig = {
+    port: PORT,
+    hot: true,
+  };
+
+  // Usamos nuestros middlewares con el servidor de express
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  // Este nos ayuda a hacer el hotmodule replacement de nuestro proyecto
+  app.use(webpackHotMiddleware(compiler));
 }
 
 app.get('*', (req, res) => {
